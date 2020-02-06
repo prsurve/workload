88f2a579c6693db30016900be468adf9354cc4b4
Pratik Surve <prsurve@redhat.com>
2020-02-06 21:44:30 +0530

    Updated Fedora image with prebuild fio (#18)

--- Deploymentconfig/run-fio-rwx.sh
+++ Deploymentconfig/run-fio-rwx.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 
-yum install fio -y &> /dev/null
-
-
 fio --name=fio-rand-readwrite --filename=/dev/rbdblock --readwrite=randrw --bs=4K --direct=1 --numjobs=1 --time_based=1 --runtime=60 --size=4G --iodepth=4 --invalidate=1 --fsync_on_close=1 --rwmixread=75 --ioengine=libaio --rate=4k --rate_process=poisson --output-format=json &> /dev/null
 
